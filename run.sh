67e98ce437d0ca60a1aa62e54aa5cff5feb1ac6e
NAlexH2 <alex@nah-red.net>
2024-05-12 11:53:39 -0700

    restoration script run

---
+++ run.sh
@@ -0,0 +1,26 @@
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im01_10.png -o 10ppp/10restored/im01_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im02_10.png -o 10ppp/10restored/im02_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im05_10.png -o 10ppp/10restored/im05_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im08_10.png -o 10ppp/10restored/im08_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im14_10.png -o 10ppp/10restored/im14_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im15_10.png -o 10ppp/10restored/im15_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im22_10.png -o 10ppp/10restored/im22_10_restored_ --use_gpu
+python colorization-master/demo_release.py -i 10ppp/10denoised/denoise_im23_10.png -o 10ppp/10restored/im23_10_restored_ --use_gpu
+
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im01_100.png -o 100ppp/100restored/im01_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im02_100.png -o 100ppp/100restored/im02_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im05_100.png -o 100ppp/100restored/im05_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im08_100.png -o 100ppp/100restored/im08_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im14_100.png -o 100ppp/100restored/im14_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im15_100.png -o 100ppp/100restored/im15_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im22_100.png -o 100ppp/100restored/im22_100_restored_ --use_gpu
+python colorization-master/demo_release.py -i 100ppp/100denoised/denoise_im23_100.png -o 100ppp/100restored/im23_100_restored_ --use_gpu
+
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im01_1000.png -o 1000ppp/1000restored/im01_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im02_1000.png -o 1000ppp/1000restored/im02_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im05_1000.png -o 1000ppp/1000restored/im05_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im08_1000.png -o 1000ppp/1000restored/im08_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im14_1000.png -o 1000ppp/1000restored/im14_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im15_1000.png -o 1000ppp/1000restored/im15_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im22_1000.png -o 1000ppp/1000restored/im22_1000_restored_ --use_gpu
+python colorization-master/demo_release.py -i 1000ppp/1000denoised/denoise_im23_1000.png -o 1000ppp/1000restored/im23_1000_restored_ --use_gpu
